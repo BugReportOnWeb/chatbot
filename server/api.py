a6830e834b7885c2cd7112e4c1ee9d5cedde8306
Dev <flashytesting@gmail.com>
2023-10-03 03:03:20 +0530

    Update host to run on all network interfaces

--- server/api.py
+++ server/api.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
+
 from controller.main import get_response
+
+import uvicorn
+
+HOST_ADDRESS = '0.0.0.0'
+PORT = 8000
 
 app = FastAPI()
 
@@ -11,4 +18,7 @@ class UserInput(BaseModel):
 def root(req: UserInput):
     res = get_response(req.msg)
     return res
+
+if __name__ == '__main__':
+    uvicorn.run(app, host=HOST_ADDRESS, port=PORT)
 

df9ce82eea7ae5d927a7e0fc84b55f54ec4531fc
Dev <flashytesting@gmail.com>
2023-10-03 03:04:43 +0530

    (minor) Update response format

--- server/controller/main.py
+++ server/controller/main.py
@@ -1,8 +1,7 @@
-def main(user_input):
+def chatbot(user_input):
     # Main chatbot logic goes here
-    return f"Hello world from 'main' for '{user_input}'"
-
+    return f"<response> from chatbot for user input '{user_input}'"
 
 def get_response(user_input):
-    res = main(user_input)
-    return res
+    res = chatbot(user_input)
+    return { 'msg': res }

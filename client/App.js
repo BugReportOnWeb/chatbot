969748313e5ad6e5e38a777c3fb5c81fce9833d6
Dev <flashytesting@gmail.com>
2023-10-02 17:45:47 +0530

    Initial commit

---
+++ client/App.js
@@ -0,0 +1,23 @@
+const { View, Text, StyleSheet } = require("react-native")
+
+const App = () => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.text}>Hello World</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    text: {
+        color: '#E5E5E5'
+    }
+})
+
+export default App;

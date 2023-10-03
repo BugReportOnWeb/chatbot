3e8ad6087888fd0d5540e8ee48180d5444b491a2
Dev <flashytesting@gmail.com>
2023-10-03 14:11:35 +0530

    Fix keyboard interruption on chat view

--- client/App.js
+++ client/App.js
@@ -1,5 +1,12 @@
-import { View, TextInput, Text } from "react-native";
 import { useState } from "react";
+import { 
+    View,
+    TextInput,
+    Text,
+    KeyboardAvoidingView,
+    Platform ,
+    ScrollView
+} from "react-native";
 
 const App = () => {
     const [inputText, setInputText] = useState('');
@@ -40,23 +47,37 @@ const App = () => {
     }
 
     return (
-        <View className='h-full px-5 py-16'>
-            <View>
-                <TextInput 
-                    className='px-3 py-3 border border-[#CCCCCC] rounded-2xl mb-5 bg-[#F5F5F5]'
-                    placeholder="Type here"
-                    defaultValue={inputText}
-                    onChangeText={newText => setInputText(newText)}
-                    onSubmitEditing={handleTextSubmit}
-                />
+        <KeyboardAvoidingView
+            behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+            keyboardVerticalOffset={-17}
+        >
+            <View className='h-full flex justify-end'>
+                <View className='px-5 mb-8'>
+                    {/* 
+                        BUG: Check on (from -> to)
+                             mb-1 (parent) and mb-2 (child)
+                             mb-3 (parent) with flex gap-2 (child) 
 
-                {chatLogs && chatLogs.map((chat, index) => (
-                    <View key={index} className={`${chat.type === 'user' ? 'self-end bg-[#2294fb]' : 'self-start bg-[#5A5A5A]'} mt-2 px-3.5 py-2.5 rounded-3xl`}>
-                        <Text className='text-white leading-5 text-base'>{chat.msg}</Text>
+                        FEAT: Scrolling on each chat section
+                    */}
+                    <View className='mb-1.5'>
+                        {chatLogs && chatLogs.map((chat, index) => (
+                            <View key={index} className={`${chat.type === 'user' ? 'self-end bg-[#0084FF]' : 'self-start bg-[#ECEFF1]'} px-3.5 py-2.5 rounded-3xl mb-2.5`}>
+                                <Text className={`${chat.type === 'user' ? 'text-white' : 'text-black'} leading-5 text-base`}>{chat.msg}</Text>
+                            </View>
+                        ))}
                     </View>
-                ))}
+
+                    <TextInput 
+                        className='px-3 py-3 border border-[#CCCCCC] rounded-2xl bg-[#F5F5F5]'
+                        placeholder="Message..."
+                        defaultValue={inputText}
+                        onChangeText={newText => setInputText(newText)}
+                        onSubmitEditing={handleTextSubmit}
+                    />
+                </View>
             </View>
-        </View>
+        </KeyboardAvoidingView>
     )
 }
 

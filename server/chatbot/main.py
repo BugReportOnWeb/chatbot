5ce54ec8303f9c72c3bdf64d276b8d9b814f31a0
Dev <flashytesting@gmail.com>
2023-10-06 19:05:53 +0530

    Clean up/format chatbot training

--- server/chatbot/main.py
+++ server/chatbot/main.py
@@ -16,7 +16,57 @@ import numpy as np
 # CHECK: Shift inside function (no global use)
 lemmatizer = WordNetLemmatizer()
 
-def chatbot(user_input):
+
+def clean_text(text):
+    tokenized_text = nltk.word_tokenize(text)
+    lemmatized_text = [lemmatizer.lemmatize(word.lower()) for word in tokenized_text if word not in string.punctuation]
+
+    return lemmatized_text
+
+
+def bag_of_words(text, vocab):
+    bow = []
+    clean_user_input = clean_text(text)
+
+    for word in vocab:
+        bow.append(1) if word in clean_user_input else bow.append(0)
+
+    return np.array(bow)
+
+
+def train_model(train_X, train_Y):
+    model = Sequential()
+
+    learning_rate_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
+        initial_learning_rate=0.01,
+        decay_steps=10000,
+        decay_rate=0.9
+    )
+
+    # CHECK: Model methods
+    # CHECK: Using legacy keras for more optimized result (optimizers.legacy.Adam)
+    # CHECK: Change of 'learning_rate' and add of 'decay_rate' (in legacy)
+    adam = tf.keras.optimizers.Adam(learning_rate=learning_rate_schedule)
+
+    model.add(Dense(128, input_shape=(len(train_X[0]),), activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(64, activation="relu"))
+    model.add(Dropout(0.5))
+    model.add(Dense(len(train_Y[0]), activation="softmax"))
+
+    model.compile(
+        loss='categorical_crossentropy',
+        optimizer=adam,
+        metrics=["accuracy"]
+    )
+
+    # CHECK: Epochs value
+    model.fit(x=train_X, y=train_Y, epochs=150, verbose=1)
+
+    return model
+
+
+def compute(user_input):
     with open('./data/intents.json', 'r') as file:
         data = json.load(file)
 
@@ -28,7 +78,7 @@ def chatbot(user_input):
     data_patterns = []
     data_tags = []
 
-    # Raw preprocessing data
+    # Building raw data 
     for intent in data["intents"]:
         for pattern in intent["patterns"]:
             tokenized_pattern = nltk.word_tokenize(pattern)
@@ -49,24 +99,17 @@ def chatbot(user_input):
     # Training data (In 0 & 1)
     # [[Match with vocab and pattern], [respective pattern tag]] 
     training = []
-    output_template = [0] * len(classes)
+    output_row_template = [0] * len(classes)
 
     for index, pattern in enumerate(data_patterns):
-        bag_of_words = []
-
-        # Preprocessing single pattern
-        tokenized_pattern = nltk.word_tokenize(pattern)
-        lemmatized_words = [lemmatizer.lemmatize(word.lower()) for word in tokenized_pattern if word not in string.punctuation] 
-
-        for word in words:
-            bag_of_words.append(1) if word in lemmatized_words else bag_of_words.append(0)
+        training_bow = bag_of_words(pattern, words)
 
         # Mathching tag from 'data_tags'
         # data_tags (obtain tag) -> classes (obtain index) -> output_row
-        output_row = list(output_template)
+        output_row = list(output_row_template)
         output_row[classes.index(data_tags[index])] = 1
     
-        training.append([bag_of_words, output_row])
+        training.append([training_bow, output_row])
 
     random.shuffle(training)
     training = np.array(training, dtype=object)
@@ -76,47 +119,13 @@ def chatbot(user_input):
     training_Y = np.array(list(training[:, 1]))
 
     # Model training
-    model = Sequential()
-
-    learning_rate_schedule = tf.keras.optimizers.schedules.ExponentialDecay(
-        initial_learning_rate=0.01,
-        decay_steps=10000,
-        decay_rate=0.9
-    )
-
-    # CHECK: Model methods
-    # CHECK: Using legacy keras for more optimized result (optimizers.legacy.Adam)
-    # CHECK: Change of 'learning_rate' and add of 'decay_rate' (in legacy)
-    adam = tf.keras.optimizers.Adam(learning_rate=learning_rate_schedule)
-
-    model.add(Dense(128, input_shape=(len(training_X[0]),), activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(64, activation="relu"))
-    model.add(Dropout(0.5))
-    model.add(Dense(len(training_Y[0]), activation="softmax"))
-
-    model.compile(
-        loss='categorical_crossentropy',
-        optimizer=adam,
-        metrics=["accuracy"]
-    )
-
-    # CHECK: Epochs value
-    model.fit(x=training_X, y=training_Y, epochs=150, verbose=1)
+    model = train_model(training_X, training_Y)
 
     # Preprocessing user input
-    tokenized_user_input = nltk.word_tokenize(user_input)
-    lemmatized_user_input = [lemmatizer.lemmatize(word) for word in tokenized_user_input if word not in string.punctuation]
+    user_input_bow = bag_of_words(user_input, words)
 
-    bag_of_words = []
-
-    for word in words:
-        bag_of_words.append(1) if word in lemmatized_user_input else bag_of_words.append(0)
-
-    bag_of_words = np.array(bag_of_words)
-
-    # Predicting and outputting result
-    result = model.predict(np.array([bag_of_words]))[0]
+    # CHECK: Redundent use of array as input
+    result = model.predict(np.array([user_input_bow]))[0]
     tag = classes[np.argmax(result)]
 
     for intent in data["intents"]:
@@ -129,5 +138,9 @@ Sorry for inconvenience! :(
 Received input: \'{user_input}\''''
 
 def get_response(user_input):
-    res = chatbot(user_input)
+    if user_input == '':
+        res = "Sorry, I don't understand"
+    else:
+        res = compute(user_input)
+
     return { 'msg': res }
